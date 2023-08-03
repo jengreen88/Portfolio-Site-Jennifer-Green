fd8679bb6fa5634e743cb90c3f85780a0373ce39
jengreen88 <137909245+jengreen88@users.noreply.github.com>
2023-08-03 12:45:00 -0400

    deleted image
    deleted image

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,6 @@
 console.log("Hello World");
 
-/*var images = document.getElementsByClassName("image-clickable");
 
-function viewLargerImage(event) {
-  var image = event.target;
-  var largerImageURL = image.getAttribute("data-large-image");
-
-  image.src = largerImageURL;
-  image.alt = "Large Image";
-  image.classList.add("larger-image");
-}
-
-for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener("click", viewLargerImage);
-}*/
 
 
 function menuToggle() {
