4d67f676e5200ca4a577b20440cda6e3dd6b4529
jengreen88 <137909245+jengreen88@users.noreply.github.com>
2023-08-04 09:48:47 -0400

    added delete file
    Needed to change IMG folder name

---
+++ img/DELETE.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+/*var images = document.getElementsByClassName("image-clickable");
+
+function viewLargerImage(event) {
+  var image = event.target;
+  var largerImageURL = image.getAttribute("data-large-image");
+
+  image.src = largerImageURL;
+  image.alt = "Large Image";
+  image.classList.add("larger-image");
+}
+
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", viewLargerImage);
+}*/
+
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+
+  <!--test-->
